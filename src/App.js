7c23fd9a8a9df06553b961d4103425865662b0b9
enginsahin61 <enginsahin101@gmail.com>
2022-12-12 19:52:59 +0300

    cart is ok

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import {
 } from 'react-router-dom';
 import Checkout from './scenes/checkout/Checkout';
 import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
 import Home from "./scenes/home/Home";
 import ItemDetails from './scenes/itemDetails/ItemDetails';
-
+import CartMenu from "./scenes/global/CartMenu"
 
 const ScrollToTop = () => {
   const {pathname} = useLocation ();
@@ -23,6 +24,7 @@ function App() {
   return (
     <div className="app">
      <BrowserRouter>
+      <Navbar />
        <ScrollToTop />
        <Routes>
         <Route path="/" element={<Home />} />
@@ -30,6 +32,7 @@ function App() {
         <Route path="checkout" element={<Checkout/>} /> 
         <Route path="checkout/success" element={<Confirmation />} />   
        </Routes>
+       <CartMenu />
      </BrowserRouter>
     </div>
   );
