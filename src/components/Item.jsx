4a0995ed19ede60801b24dfaea7bcf864ed99598
enginsahin61 <enginsahin101@gmail.com>
2022-12-15 06:54:09 +0300

    15.12

---
+++ src/components/Item.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { shades } from "../theme";
+import { addToCart } from "../state";
+import { useNavigate } from "react-router-dom";
+
+const Item = ({ item, width}) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [count, setCount] = useState(1);
+    const [isHovered, setIsCartOpen] = useState(false);
+    const {
+        palette: { neutral },
+
+    } =useTheme();
+
+    const { category, price, name, image } = item.attributes;
+    const {
+        data: {
+            attributes: {
+                formats: {
+                    medium: { url },
+                }
+            }
+        }
+    } =image;
+
+    return (
+        <Box>
+            
+        </Box>
+    )
+};
+
+
+
+export default Item;
